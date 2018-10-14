9b2e0f6193de479ea9a3e68f6a100f5bb734560f
Matthew Chan <matthew.a.chan@gmail.com>
2018-10-14 02:28:59 -0400

    Update failure and success messages

--- src/flio.ml
+++ src/flio.ml
@@ -2,4 +2,14 @@ open Ast
 
 let _ =
   let lexbuf = Lexing.from_channel stdin in
-  Parser.program Scanner.token lexbuf
+  try
+    let _p = Parser.program Scanner.token lexbuf in
+    print_endline "Parsing OK - Valid program."
+  with e ->
+    print_endline "Parsing failed - Invalid program."
+(*
+    let _err_msg = Printexc.to_string e
+    and _trace = Printexc.get_backtrace () in
+    print_endline ("Parsing failed: " ^ err_msg ^ trace);
+    raise e
+*)
