79fb55170a263cc4e092e3a37b7e10200b532399
Matt Chan <matthew.a.chan@gmail.com>
2018-11-14 02:17:32 -0500

    Enable pretty printing

--- src/flio.ml
+++ src/flio.ml
@@ -1,19 +1,18 @@
 (*
-  flio.
+  flio.ml
   Author: Matthew Chan
 *)
 
 open Ast
 
 let _ =
-let _ = print_endline "running flio" in
- let lexbuf = Lexing.from_channel stdin in
-  try
-    let ast = Parser.program Scanner.token lexbuf in
-    Semant.check ast ; print_string (Ast.string_of_program ast)
-  with e ->
-          print_endline "Parsing failed - Invalid program.";
-    raise e
+        let lexbuf = Lexing.from_channel stdin in
+        try
+                let ast = Parser.program Scanner.token lexbuf in
+                Semant.check ast ; print_string (Ast.string_of_program ast)
+        with e ->
+                print_endline "Parsing failed - Invalid program.";
+                raise e
 (*
     let _err_msg = Printexc.to_string e
     and _trace = Printexc.get_backtrace () in
