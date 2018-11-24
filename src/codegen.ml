a2e809713f1b247f82c1657702c71f2d97c15da7
Eyob <eyobtefera26@Yahoo.com>
2018-11-24 02:04:35 -0500

    Defined variable types, globals, and functions, filled out some cases for statements and expressions. See commented out code around 66-76 for some problems with filling out functions, and the for case in the pattern matching

--- src/codegen.ml
+++ src/codegen.ml
@@ -9,19 +9,48 @@ let translate program =
     let the_module = L.create_module context "flio"
     and i32_t = L.i32_type context
     and i8_t = L.i8_type context
-    (* and str_ptr_t = L.pointer_type (L.i8_type context) *)
-    (* and void_t = L.void_type context *) 
+    and str_ptr_t = L.pointer_type (L.i8_type context) 
+    and str_array = L.struct_type context [|L.array_type (L.pointer_type (L.i8_type context)) 100; L.i32_type context|]
+    and void_t = L.void_type context 
   in       
 
   (* Pattern match AST types to LLVM types *)
-  (* let ltype_of_typ = function *)
-  (*       A.Int -> i32_t *)
-  (*     | A.String -> str_ptr_t *)
-  (*     | A.Void -> void_t *)
-  (*     | A.File -> raise (Failure ("not implemented yet")) *)
-  (*     | A.Dir -> raise (Failure ("not implemented yet")) *)
-  (*     | A.Array (_, _) -> raise (Failure ("not implemented yet")) *)
-  (* in *)
+   let rec ltype_of_typ = function 
+         A.Int -> i32_t 
+       | A.String -> str_ptr_t 
+       | A.Void -> void_t 
+       | A.File -> L.struct_type context [|str_ptr_t; i32_t; str_array |]  
+       | A.Dir -> L.struct_type context [|str_ptr_t; str_array; str_array; str_array|]
+       | A.Array (typ, size) -> L.struct_type context [|L.array_type (ltype_of_typ typ) size; i32_t|]
+
+   in 
+
+   (* extract all the global variables from the statements *)
+   let globals = 
+           let global_list = List.filter (fun x -> match x with
+           A.VarDecl(_, _) -> true
+         | A.VarDeclAsn(_, _, _) -> true
+         | _ -> false) program.A.stmts
+           in List.map (fun x-> match x with
+           A.VarDecl(x, name) -> (x, name)
+         | A.VarDeclAsn(x, name, _) -> (x, name)
+         | _ -> raise(Failure("Invalid variable type"))) global_list
+
+   in
+   (* Extract the functions from the AST *)
+   let functions = program.A.funcs
+   
+   in
+
+   (*Initialize all the global variables and add them to a string map*)
+   let global_vars =
+            let global_var m (t, n) =
+                    let init = match t with
+                    _ -> L.const_int (ltype_of_typ t) 0
+             in StringMap.add n (L.define_global n init the_module) m in
+    List.fold_left global_var StringMap.empty globals 
+                    
+    in
 
   let add_terminal builder f =
     match L.block_terminator (L.insertion_block builder) with
@@ -32,16 +61,31 @@ let translate program =
   let printf_t = L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
   let printf_func = L.declare_function "printf" printf_t the_module in
 
-  (* (1* Declare the built-in printbig() function *1) *)
-  (* let printbig_t = L.function_type i32_t [| i32_t |] in *)
-  (* let printbig_func = L.declare_function "printbig" printbig_t the_module in *)
+  (* Define each function (arguments and return type) so we can call it even before we've created its body *)
 
-  (* Declare main function *)
+(* let function_decls =
+        let function_decl m fdecl =
+                let name = fdecl.A.fname 
+                and formal_types = Array.of_list (List.map (fun (t, _) -> ltype_of_typ t) fdecl.A.params)
+                in let ftype = 
+                        L.function_type (ltype_of_typ fdecl.A.typ) formal_types in
+          StringMap.add name (L.define_function name ftype the_module, fdecl) m in
+  List.fold_left function_decl StringMap.empty functions in      
+
+  let build_function_body fdecl =
+          let (the_function, _) = StringMap.find fdecl.A.fname function_decls in
+          let builder_f = L.build_at_end context (L.entry_block the_function) in *)
+  
+(* Declare main function *)
   let main_t = L.function_type i32_t [| |] in
   let main_func = L.define_function "main" main_t the_module in
 
   let builder = L.builder_at_end context (L.entry_block main_func) in
   let int_format_str = L.build_global_stringptr "%d\n" "fmt" builder in
+
+  let lookup n = try StringMap.find n global_vars 
+                 with Not_found -> raise (Failure("The variable does not exist")) 
+  in
 
   (* Build statments inside of the main function *) 
   let build_stmts s =
@@ -51,10 +95,26 @@ let translate program =
       | A.Noexpr -> L.const_int i32_t 0
       | A.FuncCall ("print", [e]) -> 
                       L.build_call printf_func [| int_format_str ; (expr builder e) |] "printf" builder
-      | A.Binop (_, _, _) -> raise (Failure ("not implemented yet"))
-      | A.Uop (_, _) -> raise (Failure ("not implemented yet"))
-      | A.StringLit _ -> raise (Failure ("not implemented yet"))
-      | A.Id _ -> raise (Failure ("not implemented yet"))
+      | A.Binop (e1, op, e2) -> let e1' = expr builder e1
+                                and e2' = expr builder e2 in
+                                (match op with
+                                A.Add -> L.build_add
+                              | A.Sub -> L.build_sub
+                              | A.Mul -> L.build_mul
+                              | A.Div -> L.build_sdiv
+                              | A.Gt -> L.build_icmp L.Icmp.Sgt
+                              | A.Lt -> L.build_icmp L.Icmp.Slt
+                              | A.Eq -> L.build_icmp L.Icmp.Eq
+                              | A.Neq -> L.build_icmp L.Icmp.Ne
+                              | A.And -> L.build_and
+                              | A.Or -> L.build_or
+                             ) e1' e2' "tmp" builder
+      | A.Uop (op, e) -> let e' = expr builder e in
+                        (match op with 
+                                A.Neg -> L.build_neg
+                              | A.Not -> L.build_not) e' "tmp" builder
+      | A.StringLit s -> L.build_global_stringptr s "string" builder
+      | A.Id s -> L.build_load (lookup s) s builder
       | A.FuncCall(_, _) -> raise (Failure ("not implemented yet"))
       | A.ArrAccess (_, _) -> raise (Failure ("not implemented yet"))
       | A.ArrLit _ -> raise (Failure ("not implemented yet"))
@@ -65,15 +125,28 @@ let translate program =
     let rec stmt builder = function
         A.Block sl -> List.fold_left stmt builder sl
       | A.Expr e -> ignore (expr builder e); builder
-      | A.Nostmt -> raise (Failure ("not implemented yet"))
+      | A.Nostmt -> builder
       | A.For (_, _, _, _) -> raise (Failure ("not implemented yet")) 
       | A.Foreach (_, _, _) -> raise (Failure ("not implemented yet"))
-      | A.If (_, _, _) -> raise (Failure ("not implemented yet"))
+      | A.If (predicate, then_stmt, else_stmt) -> 
+       (* let bool_val = expr builder predicate in
+        let merge_bb = L.append_block context "merge" the_function in
+        let b_br_merge = L.build_br merge_bb in
+        let then_bb = L.append_block context "then" the_function in
+        add_terminal(stmt (L.builder_at_end context then_bb) then_stmt)
+        b_br_merge;
+
+        let else_bb = L.append_block context "else" the_function in
+        add_terminal (stmt (L.builder_at_end context else_bb) else_stmt) b_br_merge;
+        ignore(L.build_cond_br bool_val then_bb else_bb builder);
+
+        L.builder_at_end context merge_bb*)
       | A.Elif (_, _) -> raise (Failure ("not implemented yet"))
-      | A.Return _ -> raise (Failure ("not implemented yet"))
-      | A.VarDecl (_, _) -> raise (Failure ("not implemented yet"))
-      | A.VarDeclAsn (_, _, _) -> raise (Failure ("not implemented yet"))
-      | A.Asn (_, _) -> raise (Failure ("not implemented yet"))
+      | A.Return _  -> raise(Failure ("not implemented yet"))
+      | A.VarDecl (_, _) -> builder
+      | A.VarDeclAsn (_, name, expr) -> ignore (stmt builder (A.Asn(name, expr))); builder
+      | A.Asn (s, e) ->  let e' = expr builder e in ignore(L.build_store e' (lookup s) builder); builder
+
     in
 
     (* Build the code for each statement in the function *)
