0cc59dd4349e1ceab642d6d3c5dfc29cc697093d
Matt Chan <matthew.a.chan@gmail.com>
2018-12-12 17:55:36 -0500

    Implemented functions

--- src/codegen.ml
+++ src/codegen.ml
@@ -5,6 +5,7 @@ module StringMap = Map.Make(String)
 
 let translate program =
   
+  (* Setup LLVM environment vars *)
   let context = L.global_context () in
     let the_module = L.create_module context "flio"
     and i32_t = L.i32_type context
@@ -23,17 +24,12 @@ let translate program =
   (*     | A.Array (_, _) -> raise (Failure ("not implemented yet")) *)
   in
 
-    (* Return the value for a variable or formal argument *)
-    let lookup n m = StringMap.find n m
-    in
+  (* Utility function for getting a val from a map, given a key *)
+  let lookup n m = StringMap.find n m
+  in
 
-  (* Declare each global variable; remember its value in a map *)
-  let main_vars = StringMap.empty in 
-    (* let global_var m (t, n) = *)
-    (*   let init = L.const_int (ltype_of_typ t) 0 *)
-    (*   in StringMap.add n (L.define_global n init the_module) m in *)
-    (* List.fold_left global_var StringMap.empty globals in *)
 
+  (* Utility function to build a return block *)
   let add_terminal builder f =
     match L.block_terminator (L.insertion_block builder) with
         Some _ -> ()
@@ -43,15 +39,159 @@ let translate program =
   let printf_t = L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
   let printf_func = L.declare_function "printf" printf_t the_module in
 
-  (* (1* Declare the built-in printbig() function *1) *)
-  (* let printbig_t = L.function_type i32_t [| i32_t |] in *)
-  (* let printbig_func = L.declare_function "printbig" printbig_t the_module in *)
+  (* Build a map of function declarations *)
+  let function_decls =
+    let function_decl m fdecl = 
+      let name = fdecl.A.fname
+      and formal_types =
+        Array.of_list (List.map (fun (t, _) -> ltype_of_typ t) fdecl.A.params)
+      in let ftype = L.function_type (ltype_of_typ fdecl.A.typ) formal_types in
+      StringMap.add name (L.define_function name ftype the_module, fdecl) m in
+    List.fold_left function_decl StringMap.empty program.A.funcs in
+
+  (* Build function bodies *)
+  let build_function_body fdecl =
+    (* Find the function in our map *)
+    let (fdef, _) = StringMap.find fdecl.A.fname function_decls in
+    (* Move the builder to the entry point of that function *)
+    let builder = L.builder_at_end context (L.entry_block fdef) in
+
+    (* Default format strings *)
+    let int_format_str = L.build_global_stringptr "%d\n" "fmt" builder in
+    let str_format_str = L.build_global_stringptr "%s\n" "fmt" builder in
+
+    (* The function's local variables are initially the function args *)
+    let add_formal m (t, n) p = L.set_value_name n p;
+      let local = L.build_alloca (ltype_of_typ t) n builder in
+      ignore (L.build_store p local builder);
+      StringMap.add n local m in
+    let local_vars = List.fold_left2 add_formal StringMap.empty 
+      fdecl.A.params (Array.to_list (L.params fdef)) in
+
+    (* Construct code for an expression; return its value *)
+    let rec fexpr map builder = function
+        A.IntLit i -> L.const_int i32_t i
+      | A.Noexpr -> L.const_int i32_t 0
+      | A.FuncCall ("print", [e]) -> 
+              L.build_call printf_func [| int_format_str ; 
+              (fexpr map builder e) |] "printf" builder
+      | A.FuncCall ("prints", [e]) -> 
+              L.build_call printf_func [| str_format_str; 
+              (fexpr map builder e) |] "printf" builder
+      | A.Binop (e1, op, e2) -> 
+	  let e1' = fexpr map builder e1
+	  and e2' = fexpr map builder e2 in
+	  (match op with
+	    A.Add     -> L.build_add
+	  | A.Sub     -> L.build_sub
+	  | A.Mul    -> L.build_mul
+          | A.Div     -> L.build_sdiv
+	  | A.And     -> L.build_and
+	  | A.Or      -> L.build_or
+	  | A.Eq   -> L.build_icmp L.Icmp.Eq
+	  | A.Neq     -> L.build_icmp L.Icmp.Ne
+	  | A.Lt    -> L.build_icmp L.Icmp.Slt
+	  | A.Gt -> L.build_icmp L.Icmp.Sgt
+	  ) e1' e2' "tmp" builder
+      | A.Uop (_, _) -> raise (Failure ("not implemented yet"))
+      | A.StringLit s -> 
+              L.build_global_stringptr s "strptr" builder
+      | A.Id s -> L.build_load (lookup s map) s builder 
+      | A.FuncCall(f, args) -> 
+                let (fdef, fdecl) = StringMap.find f function_decls in
+                let actuals = List.rev (List.map (fexpr map builder) (List.rev args)) in
+                let result = (match fdecl.A.typ with
+                  A.Void -> ""
+                  | _ -> f ^ "_result") in
+                L.build_call fdef (Array.of_list actuals) result builder
+      | A.ArrAccess (_, _) -> raise (Failure ("not implemented yet"))
+      | A.ArrLit _ -> raise (Failure ("not implemented yet"))
+    in
+
+    let rec fstmt mb = function
+        A.Block sl -> (List.fold_left fstmt mb sl)
+      | A.Expr e -> ignore (fexpr (fst mb) (snd mb) e); mb
+      | A.Nostmt -> mb 
+      | A.For (s1, e, s2, body) -> 
+                (* Construct for basic block *)
+                let init_bb = L.append_block context "init" fdef in
+                ignore (L.build_br init_bb (snd mb)); 
+                
+                let pred_bb = L.append_block context "for" fdef in
+                pred_bb;
+
+                let init = fstmt (fst mb, L.builder_at_end context init_bb) s1 in
+                add_terminal (snd init) (L.build_br pred_bb);
+
+                (* Construct body basic block, and add s2 at the tail *)
+                let body_bb = L.append_block context "for_body" fdef in
+                let b = (fstmt (fst mb, L.builder_at_end context body_bb) body) in
+                add_terminal (snd (fstmt b s2)) (L.build_br pred_bb);
+
+                (* Do initialization before checking the predicate e *)
+                let pred_builder = L.builder_at_end context pred_bb in
+                let bool_val = fexpr (fst init) pred_builder e in
+
+                (* Construct merge basic block *)
+                let merge_bb = L.append_block context "merge" fdef in
+                ignore (L.build_cond_br bool_val body_bb merge_bb pred_builder);
+                (fst mb, L.builder_at_end context merge_bb)
+      | A.Foreach (_, _, _) -> raise (Failure ("not implemented yet"))
+      | A.If (p, then_stmt, else_stmt) -> 
+         let bool_val = fexpr (fst mb) builder p in
+	 let merge_bb = L.append_block context "merge" fdef in
+
+	 let then_bb = L.append_block context "then" fdef in
+	 add_terminal (snd (fstmt (fst mb, L.builder_at_end context then_bb) then_stmt))
+	   (L.build_br merge_bb);
+
+	 let else_bb = L.append_block context "else" fdef in
+	 add_terminal (snd (fstmt (fst mb, L.builder_at_end context else_bb) else_stmt))
+	   (L.build_br merge_bb);
+
+	 ignore (L.build_cond_br bool_val then_bb else_bb builder);
+	  (fst mb, L.builder_at_end context merge_bb)
+      | A.Elif (_, _) -> raise (Failure ("not implemented yet"))
+      | A.Return _ -> raise (Failure ("not implemented yet"))
+      | A.VarDecl (t, n) -> let init = 
+              (match t with
+                  A.Int -> L.build_alloca i32_t n (snd mb) 
+                | A.String -> L.build_alloca str_ptr_t n (snd mb)
+              ) in
+              ((StringMap.add n init (fst mb)), snd mb)
+      | A.VarDeclAsn (t, s, e) -> let init = 
+              (match t with
+                  A.Int -> L.build_alloca i32_t s (snd mb) 
+                | A.String -> L.build_alloca str_ptr_t s (snd mb)
+              ) in
+              let m = (StringMap.add s init (fst mb)) in
+              let e' = fexpr (m) (snd mb) e in
+              ignore(L.build_store e' (lookup s (m)) (snd mb)) ; (m, (snd mb))
+      | A.Asn (s, e) -> let e' = fexpr (fst mb) (snd mb) e in
+        ignore(L.build_store e' (lookup s (fst mb)) (snd mb)) ; mb
+    in
+    
+    let builder = (snd (fstmt (local_vars, builder) (A.Block fdecl.A.body))) 
+    in
+    (* Add a return if the last block falls off the end *)
+    add_terminal builder (match fdecl.A.typ with
+        A.Void -> L.build_ret_void
+      | t -> L.build_ret (L.const_int (ltype_of_typ t) 0))
+  in
+
+
 
   (* Declare main function *)
   let main_t = L.function_type i32_t [| |] in
   let main_func = L.define_function "main" main_t the_module in
 
+  (* Variables in main are the empty set initially *)
+  let main_vars = StringMap.empty in 
+
+  (* Init builder inside of main *)
   let builder = L.builder_at_end context (L.entry_block main_func) in
+
+  (* Default format strings *)
   let int_format_str = L.build_global_stringptr "%d\n" "fmt" builder in
   let str_format_str = L.build_global_stringptr "%s\n" "fmt" builder in
 
@@ -62,9 +202,11 @@ let translate program =
         A.IntLit i -> L.const_int i32_t i
       | A.Noexpr -> L.const_int i32_t 0
       | A.FuncCall ("print", [e]) -> 
-                      L.build_call printf_func [| int_format_str ; (expr map builder e) |] "printf" builder
+              L.build_call printf_func [| int_format_str ; 
+              (expr map builder e) |] "printf" builder
       | A.FuncCall ("prints", [e]) -> 
-                      L.build_call printf_func [| str_format_str; (expr map builder e) |] "printf" builder
+              L.build_call printf_func [| str_format_str; 
+              (expr map builder e) |] "printf" builder
       | A.Binop (e1, op, e2) -> 
 	  let e1' = expr map builder e1
 	  and e2' = expr map builder e2 in
@@ -82,23 +224,25 @@ let translate program =
 	  ) e1' e2' "tmp" builder
       | A.Uop (_, _) -> raise (Failure ("not implemented yet"))
       | A.StringLit s -> 
-                      L.build_global_stringptr s "strptr" builder
-                      (* L.const_string context s *) 
+              L.build_global_stringptr s "strptr" builder
       | A.Id s -> L.build_load (lookup s map) s builder 
-      | A.FuncCall(_, _) -> raise (Failure ("not implemented yet"))
+      | A.FuncCall(f, args) -> 
+                let (fdef, fdecl) = StringMap.find f function_decls in
+                let actuals = List.rev (List.map (expr map builder) (List.rev args)) in
+                let result = (match fdecl.A.typ with
+                  A.Void -> ""
+                  | _ -> f ^ "_result") in
+                L.build_call fdef (Array.of_list actuals) result builder
       | A.ArrAccess (_, _) -> raise (Failure ("not implemented yet"))
       | A.ArrLit _ -> raise (Failure ("not implemented yet"))
     in
 
-    (* Build the code for the given statement; return the builder for
-       the statement's successor *)
+    (* Build the code for the given statement; return the StringMap and builder 
+       for the statement's successor *)
     let rec stmt mb = function
-        A.Block sl -> 
-                (* stmt map builder (List.hd sl) *)
-                (* let b = (map, builder) in *)
-        (List.fold_left stmt mb sl)
+        A.Block sl -> (List.fold_left stmt mb sl)
       | A.Expr e -> ignore (expr (fst mb) (snd mb) e); mb
-      | A.Nostmt -> raise (Failure ("not implemented yet"))
+      | A.Nostmt -> mb 
       | A.For (s1, e, s2, body) -> 
                 (* Construct for basic block *)
                 let init_bb = L.append_block context "init" main_func in
@@ -140,19 +284,20 @@ let translate program =
 	  (fst mb, L.builder_at_end context merge_bb)
       | A.Elif (_, _) -> raise (Failure ("not implemented yet"))
       | A.Return _ -> raise (Failure ("not implemented yet"))
-      | A.VarDecl (t, n) -> let init = (match t with
-                        A.Int -> L.build_alloca i32_t n (snd mb) 
-                      | A.String -> L.build_alloca str_ptr_t n (snd mb)
-                ) in
+      | A.VarDecl (t, n) -> let init = 
+              (match t with
+                  A.Int -> L.build_alloca i32_t n (snd mb) 
+                | A.String -> L.build_alloca str_ptr_t n (snd mb)
+              ) in
               ((StringMap.add n init (fst mb)), snd mb)
-      | A.VarDeclAsn (t, s, e) -> let init = (match t with
-                        A.Int -> L.build_alloca i32_t s (snd mb) 
-                      | A.String -> L.build_alloca str_ptr_t s (snd mb)
-                ) in
-                let m = (StringMap.add s init (fst mb)) in
+      | A.VarDeclAsn (t, s, e) -> let init = 
+              (match t with
+                  A.Int -> L.build_alloca i32_t s (snd mb) 
+                | A.String -> L.build_alloca str_ptr_t s (snd mb)
+              ) in
+              let m = (StringMap.add s init (fst mb)) in
               let e' = expr (m) (snd mb) e in
-                ignore(L.build_store e' (lookup s (m)) (snd mb)) ; (m, (snd mb))
-
+              ignore(L.build_store e' (lookup s (m)) (snd mb)) ; (m, (snd mb))
       | A.Asn (s, e) -> let e' = expr (fst mb) (snd mb) e in
         ignore(L.build_store e' (lookup s (fst mb)) (snd mb)) ; mb
     in
@@ -162,6 +307,9 @@ let translate program =
           add_terminal builder (L.build_ret (L.const_int i32_t 0))
   in
 
+
+  ignore(List.iter build_function_body program.A.funcs);
   ignore(build_stmts program.A.stmts);
+
   (* Add terminal for main function *)
   the_module
